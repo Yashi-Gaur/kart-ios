d2b70e5e7102d67c80197066ee5e1aa5d42b9ac9
Yashi Gaur <yashigaurbcgs@gmail.com>
2026-03-27 13:29:07 +0530

    Initial Commit

---
+++ Kart/KartApp.swift
@@ -0,0 +1,17 @@
+//
+//  KartApp.swift
+//  Kart
+//
+//  Created by Yashi Gaur on 27/03/26.
+//
+
+import SwiftUI
+
+@main
+struct KartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ KartTests/KartTests.swift
@@ -0,0 +1,17 @@
+//
+//  KartTests.swift
+//  KartTests
+//
+//  Created by Yashi Gaur on 27/03/26.
+//
+
+import Testing
+@testable import Kart
+
+struct KartTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
